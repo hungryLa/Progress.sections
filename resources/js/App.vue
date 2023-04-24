4998d675aa015c307170dec21d78740753bc15a8
hungryLa <hungry_la@mail.ru>
2023-04-24 19:03:07 +0500

    Загрузка проекта

---
+++ resources/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <Header/>
+    <router-view></router-view>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+    name: "App",
+    components: {Header}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+        <nav>
+            <ul>
+              <li><router-link to="/">Main</router-link></li>
+              <li><router-link to="/about">About</router-link></li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
