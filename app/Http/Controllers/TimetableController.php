53cee28b339de63a4dece8b025c84e569148aeb6
niyaz <hungry_la@mail.ru>
2023-05-02 15:39:07 +0500

    Добавил методы для TimetableController.php

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\Timetable\TimetableResource;
 use App\Models\Teacher;
+use App\Models\Timetable;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -13,5 +14,57 @@ class TimetableController extends Controller
     public function index(Teacher $teacher){
         $data['timetables'] = TimetableResource::collection($teacher->timetables);
         return $data;
+    }
+
+    public function store(Request $request, Teacher $teacher){
+        try {
+            $success = Timetable::create([
+                'teacher_id' => $teacher->id,
+                'days_week' => $request->days_week,
+                'lesson_time' => $request->lesson_time,
+                'workday_start' => $request->workday_start,
+                'workday_end' => $request->workday_end,
+                'rest' => $request->rest,
+                'rest_start' => $request->rest_start,
+                'rest_end' => $request->rest_end,
+            ]);
+            if($success) {
+                session()->flash('success', __('other.Record successfully added'));
+            }
+        }catch (\Exception $exception){
+            return $exception->getMessage();
+        }
+    }
+
+    public function edit(Request $request, Teacher $teacher, Timetable $timetable){
+        try {
+            $success = $timetable->update([
+                'teacher_id' => $teacher->id,
+                'days_week' => $request->days_week,
+                'lesson_time' => $request->lesson_time,
+                'workday_start' => $request->workday_start,
+                'workday_end' => $request->workday_end,
+                'rest' => $request->rest,
+                'rest_start' => $request->rest_start,
+                'rest_end' => $request->rest_end,
+            ]);
+            if($success){
+                session()->flash('success',__('other.Information changed successfully'));
+            }
+        }catch (\Exception $exception){
+            return $exception->getMessage();
+        }
+    }
+
+    public function destroy(Teacher $teacher, Timetable $timetable){
+        try {
+            $success = $timetable->delete();
+            if($success){
+                session()->flash('success',__('other.The record was successfully deleted'));
+            }
+        }
+        catch (\Exception $exception){
+            return $exception->getMessage();
+        }
     }
 }
